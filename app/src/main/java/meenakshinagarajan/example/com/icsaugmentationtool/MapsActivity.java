7787483ffdf25ab65b291eb3b5e775dd71fa07c2
meenakshinagarajan <nagarajan.12@wright.edu>
2017-10-16 00:25:35 -0400

    10/16/2017 commit

--- app/src/main/java/meenakshinagarajan/example/com/icsaugmentationtool/MapsActivity.java
+++ app/src/main/java/meenakshinagarajan/example/com/icsaugmentationtool/MapsActivity.java
@@ -1,10 +1,16 @@
 package meenakshinagarajan.example.com.icsaugmentationtool;
 
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,8 +19,11 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -24,7 +33,14 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     List<Address> address = null;
     private double yourLatitude = 39.7845;
     private double yourLongitude = -84.0580;
+    private double incidentLatitude = 39.533860;
+    private double incidentLongitude = -84.371423;
+    LatLng incidentLocation = new LatLng(incidentLatitude, incidentLongitude);
+    LatLng yourLocation = new LatLng(yourLatitude, yourLongitude);
+    ArrayList<LatLng> points;
+    PolylineOptions lineOptions = null;
     Geocoder geocoder;
+    List<Address> incidentAddress = getAddress(incidentLatitude,incidentLongitude);
 
 
     @Override
@@ -44,8 +60,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
 
     }
-
-
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -58,15 +72,50 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         // Add a marker in Sydney and move the camera
-        LatLng yourLocation = new LatLng(39.7845, -84.0580);
         Geocoder geocoder = new Geocoder(this, Locale.getDefault());
         address = getAddress(yourLatitude, yourLongitude);
         Marker currentLocationMarker=mMap.addMarker(new MarkerOptions().position(yourLocation).title("You are here:" + address.get(0).getAddressLine(0)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(yourLocation));
         mMap.animateCamera(CameraUpdateFactory.zoomTo(14), 2000, null);
         currentLocationMarker.showInfoWindow();
+
+        //declarations
+        final CardView alertCard = (CardView) findViewById(R.id.alertCard);
+        final TextView alertText = (TextView) findViewById(R.id.alertText);
+        final Button takeActionButton = (Button) findViewById(R.id.takeActionButton);
+
+
+
+        //alertbox after 5 seconds
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                alertCard.setVisibility(View.VISIBLE);
+                alertText.setText("Oh Snap! Vehicular accident at "+incidentAddress.get(0).getAddressLine(0));
+                takeActionButton.setVisibility(View.VISIBLE);
+            }
+        }, 10000);
+
+
+        //after click of takeaction button
+        takeActionButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+
+                Marker incidentLocationMarker=mMap.addMarker(new MarkerOptions().position(incidentLocation).title("Incident Location:" +incidentAddress.get(0).getAddressLine(0)));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(incidentLocation));
+                mMap.animateCamera(CameraUpdateFactory.zoomTo(8), 2000, null);
+                incidentLocationMarker.showInfoWindow();
+                Polyline line = mMap.addPolyline(new PolylineOptions()
+                                .add(new LatLng(yourLatitude, yourLongitude), new LatLng(incidentLatitude,   incidentLongitude))
+                                .width(2).color(Color.RED).geodesic(true));
+            }
+        });
+
+
+
 
     }
 
